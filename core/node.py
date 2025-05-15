ca05628dcc041c0e7b110add0dc7c318a376f85d
Hans Queja <hansqueja8@gmail.com>
2025-05-15 22:27:01 +0800

    Add node class for a* nodes

---
+++ core/node.py
@@ -0,0 +1,13 @@
+
+class Node:
+    def __init__(self):
+        # Parent cell's row index
+        self.parent_x = 0
+        # Parent cell's column index
+        self.parent_y = 0
+        # Total cost of the cell (g + h)
+        self.f = float('inf')
+        # Cost from start to this cell
+        self.g = float('inf')
+        # Heuristic cost from this cell to destination
+        self.h = 0
